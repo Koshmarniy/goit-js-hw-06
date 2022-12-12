9ecbc3abad83d4b073d5a05c1add6125ed44640f
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2022-12-12 16:03:09 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event){
+event.preventDefault();
+
+const formData = new FormData(event.currentTarget);
+
+formData.forEach((value, name) => {
+    console.log('onFormSubmit ->', name)
+    console.log('onFormSubmit -> value', value);
+    
+})
+console.log([formData])
+}
