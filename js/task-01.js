96d95a5c7e0b92a91f48fae1952d34fb3a76fb3d
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2022-12-13 08:45:17 +0200

    add task#9

--- js/task-01.js
+++ js/task-01.js
@@ -12,7 +12,6 @@ console.log('Category:', findFirstH)
 const findFirstHLength = navEl[0].lastElementChild.children.length;
 console.log(findFirstHLength)
 
-
 ///////////////////////////////////////////////////
 const findSecondH = navEl[1].firstElementChild.textContent;
 console.log('Category:', findSecondH)
@@ -20,11 +19,9 @@ console.log('Category:', findSecondH)
 const findSecondHLength = navEl[1].lastElementChild.children.length;
 console.log(findSecondHLength)
 
-
 //////////////////////////////////////////////////////////
 const findLastH = navEl[2].firstElementChild.textContent;
 console.log('Category:', findLastH)
-
 
 const findLastHLength = navEl[2].lastElementChild.children.length;
 console.log(findLastHLength)
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const btnChangeColor = document.querySelector('.change-color')
+const spanColor = document.querySelector('.color')
+
+
+btnChangeColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+  
+})
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
